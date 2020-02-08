1a7eb50c1f167e1bb3bdf69089937c354d9a8d90
Liopin <contact.pinheiro.lionel@gmail.com>
2020-02-08 16:25:03 +0100

    urls done, template stated

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='website-home'),
+    path('services/', views.services, name='website-services'),
+    path('équipe/', views.team, name='website-team'),
+    path('contact/', views.contact, name='website-contact'),
     
 ]
--- website/views.py
+++ website/views.py
@@ -6,3 +6,12 @@ from django. http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>home<h1>')
+
+def services(request):
+    return HttpResponse('<h1>services<h1>')
+
+def team(request):
+    return HttpResponse('<h1>equipes<h1>')
+
+def contact(request):
+    return HttpResponse('<h1>contact<h1>')
